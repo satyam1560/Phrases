b2a9aea8615f1464afd428c064b16cb25a2262c7
Satyam Chourasia <satyam.20ch@gmail.com>
2023-08-05 13:43:18 +0530

    1.0 app theme

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:advicer/3_application/core/services/theme_service.dart';
+import 'package:advicer/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeService(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(builder: (context, themeService, child) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: const Placeholder(),
+      );
+    });
+  }
+}
